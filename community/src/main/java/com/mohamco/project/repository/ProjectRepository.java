11184030e35ebc86d982b783c448fbe6edfbcc0a
박수진 <soojin_expo@naver.com>
2023-08-20 16:42:50 +0900

    settings

---
+++ community/src/main/java/com/mohamco/project/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.mohamco.project.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository {
+}
---
+++ community/src/main/java/com/mohamco/study/service/StudyService.java
@@ -0,0 +1,7 @@
+package com.mohamco.study.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudyService {
+}
