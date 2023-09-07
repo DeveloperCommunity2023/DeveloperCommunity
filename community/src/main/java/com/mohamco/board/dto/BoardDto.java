7a7d69b604e3ea5c0231b6f6126ffca296cfe52d
박수진 <soojin_expo@naver.com>
2023-09-07 23:18:41 +0900

    Feat: 게시판 폴더 추가

---
+++ community/src/main/java/com/mohamco/board/dto/BoardDto.java
@@ -0,0 +1,8 @@
+package com.mohamco.board.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BoardDto {
+
+}
---
+++ community/src/main/java/com/mohamco/board/service/BoardService.java
@@ -0,0 +1,7 @@
+package com.mohamco.board.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+}
