2dbcc934da30264eaf47a043d0a800bc7be23f29
hwayoung <lweyine.sm@gmail.com>
2023-11-20 16:32:03 +0900

    FEAT: 프로젝트 메인 api ì¶추가

--- community/src/main/java/com/mohamco/project/controller/ProjectController.java
+++ community/src/main/java/com/mohamco/project/controller/ProjectController.java
@@ -1,8 +1,25 @@
 package com.mohamco.project.controller;
 
+import com.mohamco.common.response.BaseResponse;
+import com.mohamco.project.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ProjectController {
+  private final ProjectService projectService;
+
+  /**
+   *
+   * @return 프로젝트 리스트를 반환한다.
+   */
+  @GetMapping("/v1/project/main")
+  public ResponseEntity<?> getProject(){
+    return ResponseEntity.ok(BaseResponse.builder()
+            .data(projectService.getProjectList()).build());
+  }
 
 }
--- community/src/main/java/com/mohamco/project/service/ProjectService.java
+++ community/src/main/java/com/mohamco/project/service/ProjectService.java
@@ -1,8 +1,24 @@
 package com.mohamco.project.service;
 
+import com.mohamco.project.dto.ProjectDto;
+import com.mohamco.project.entity.ProjectEntity;
+import com.mohamco.project.repository.ProjectRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectService {
+  private final ProjectRepository projectRepository;
+
+  public ProjectDto.Response getProjectList() {
+    List<ProjectEntity> p = projectRepository.findAllByOpenYn(1);
+
+    return ProjectDto.Response.builder()
+            .projectList(p.stream().map(ProjectDto.ProjectList::of).collect(Collectors.toList()))
+            .build();
+  }
 
 }
