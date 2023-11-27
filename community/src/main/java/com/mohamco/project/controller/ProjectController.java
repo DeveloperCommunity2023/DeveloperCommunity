f47afce4c148f26491d4c90feeea36091261a240
hwayoung <lweyine.sm@gmail.com>
2023-11-27 16:59:10 +0900

    Feat: 프롲ㄹ러ã로젝트 상세 apiã 추가

--- community/src/main/java/com/mohamco/project/controller/ProjectController.java
+++ community/src/main/java/com/mohamco/project/controller/ProjectController.java
@@ -5,6 +5,7 @@ import com.mohamco.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,9 +18,19 @@ public class ProjectController {
    * @return 프로젝트 리스트를 반환한다.
    */
   @GetMapping("/v1/project/main")
-  public ResponseEntity<?> getProject() {
+  public ResponseEntity<?> getProjectList() {
     return ResponseEntity.ok(BaseResponse.builder()
             .data(projectService.getProjectList()).build());
+  }
+
+  /**
+   * @param projectSeq (게시글 아이디)
+   * @return 프로젝트 상세를 반환한다.
+   */
+  @GetMapping("/v1/project/{projectSeq}")
+  public ResponseEntity<?> getProject(@PathVariable("projectSeq") Long projectSeq) {
+    return ResponseEntity.ok(BaseResponse.builder()
+            .data(projectService.getProject(projectSeq)).build());
   }
 
 }
--- community/src/main/java/com/mohamco/project/dto/ProjectDto.java
+++ community/src/main/java/com/mohamco/project/dto/ProjectDto.java
@@ -7,16 +7,27 @@ import lombok.Getter;
 
 @Getter
 public class ProjectDto {
-
   @Builder
   @Getter
-  public static class Response {
-    List<ProjectDto.ProjectList> projectList;
+  public static class CommonResponse {
+    private Long projectSeq; // 프로젝트 아이디
+    private String projectName; // 프로젝트 이름
+    private String projectDesc; // 프로젝트 설명
+    private Integer projectCount; // 프로젝트 인원 수
+    private String profileUrl; // 프로젝트 프로필 이미지
+    private String headerUrl; // 프로젝트 헤더 이미지
+    private Integer recruitYn; // 프로젝트 모집 여부
   }
 
   @Builder
   @Getter
-  public static class ProjectList { // 프로젝트 리스트
+  public static class Response {
+    List<Project> projectList;
+  }
+
+  @Builder
+  @Getter
+  public static class Project { // 프로젝트 Dto
     private Long projectSeq; // 프로젝트 아이디
     private String projectName; // 프로젝트 이름
     private String projectDesc; // 프로젝트 설명
@@ -26,8 +37,8 @@ public class ProjectDto {
     private Integer recruitYn; // 프로젝트 모집 여부
 
 
-    public static ProjectDto.ProjectList of(ProjectEntity projectEntity) {
-      return ProjectDto.ProjectList.builder()
+    public static Project of(ProjectEntity projectEntity) {
+      return Project.builder()
               .projectSeq(projectEntity.getProjectSeq())
               .projectName(projectEntity.getProjectName())
               .projectDesc(projectEntity.getProjectDesc())
